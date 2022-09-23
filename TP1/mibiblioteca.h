50f90b8ddd14289afe84a9b0cf5253f0d607e268
MatiasCripto <elpollysca87@gmail.com>
2022-09-23 09:59:36 -0300

    doxygen completo

--- TP1/mibiblioteca.h
+++ TP1/mibiblioteca.h
@@ -20,20 +20,68 @@
 
 #endif // MENUPRINCIPAL_H_INCLUDED
 
-
+/** \brief confirma la carga y muestra actualizado los gastos y jugadores ingresados
+ *
+ * \param pOpciones int* almacena la opcion ingresada del menu principal
+ * \param acumUno int acumula los gastos ingresados
+ * \param acumDos int acumula los gastos ingresados
+ * \param acumTres int acumula los gastos ingresados
+ * \param contUno int cuenta los jugadores de cada posicion
+ * \param contDos int cuenta los jugadores de cada posicion
+ * \param contTres int cuenta los jugadores de cada posicion
+ * \param contCuatro int cuenta los jugadores de cada posicion
+ * \return void no retorna nada
+ *
+ */
 void menu( int* pOpciones,int acumUno, int acumDos, int acumTres, int contUno, int contDos, int contTres, int contCuatro);
 
+/** \brief submenu de gastos de mantenimiento
+ *
+ * \param op1 int* almacena la opcion ingresada
+ * \return void
+ *
+ */
 void menuMantenimiento(int* op1);
 
+/** \brief recibe el costo y lo acumula
+ *
+ * \param pAcumCosto int* acumulador de costos
+ * \return void
+ *
+ */
 void menuCostos(int* pAcumCosto);
 
+/** \brief menu de carga de confederaciones
+ *
+ * \param opcion int* almacena la opcion ingresada
+ * \return int retorna 1 o 0
+ *
+ */
 int cargaConfederaciones(int* opcion);
 
+/** \brief valida a el caracter 's' para que sea la unica opcion que se debe ingresar si se quiere salir del programa
+ *
+ * \param pSalida char* asignacion de caracter
+ * \return int
+ *
+ */
 int confirmarSalida(char* pSalida);
 
+/** \brief menu para la carga de las distintas posiciones de los jugadores
+ *
+ * \param pOpciones int* almacena la opcion ingresada
+ * \return void
+ *
+ */
 void cargaJugadores(int* pOpciones);
 
+/** \brief divide dos numeros validando que uno no sea 0
+ *
+ * \param pResultado float* donde se almacena el resultado
+ * \param dividendo int ingreso del divisor
+ * \param divisor int ingreso del dividendo != 0
+ * \return int
+ *
+ */
 int dividir(float* pResultado, int dividendo, int divisor);
-
-
 
